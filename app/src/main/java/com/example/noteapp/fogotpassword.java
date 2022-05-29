7be4a0dedd73581e6a287b1710148837998148d9
Ad <sabrietihad@gmail.com>
2022-05-29 19:56:31 +0800

    validasi data ( sabri )

--- app/src/main/java/com/example/noteapp/fogotpassword.java
+++ app/src/main/java/com/example/noteapp/fogotpassword.java
@@ -2,13 +2,53 @@ package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class fogotpassword extends AppCompatActivity {
+
+    private EditText mforgotpassword;
+    private Button mpasswordrecoverbutton;
+    private TextView mgobacktologin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fogotpassword);
+
+        getSupportActionBar().hide();
+
+        mforgotpassword=findViewById(R.id.forgotpassword);
+        mpasswordrecoverbutton=findViewById(R.id.passwordrecoverbutton);
+        mgobacktologin=findViewById(R.id.gobacktologin);
+
+        mgobacktologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(fogotpassword.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mpasswordrecoverbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail=mforgotpassword.getText().toString().trim();
+                if (mail.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Enter Your Mail First", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    //we have to send password recover email
+                }
+            }
+        });
+
     }
 }
